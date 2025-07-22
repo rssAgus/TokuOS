2343896651da0f6b597eebc5a37c0dfd2840d776
rssAgus <arusso@fixear.net>
2025-07-21 23:09:09 -0300

    ahi va

--- hosts/default.nix
+++ hosts/default.nix
@@ -27,7 +27,7 @@
   };
 
   services.xserver.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.desktopManager.plasma6.enable = true;
   services.xserver.displayManager.sddm.enable = true;
 
   environment.systemPackages = with pkgs; [

c44cca23343a266b9edcf7fba083d589495997ba
rssAgus <arusso@fixear.net>
2025-07-22 14:01:35 -0300

    Code order 8)

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,46 +1,55 @@
 { config, pkgs, ... }:
 
 {
-  networking.hostName = "TokuOS";
+  # Hostname :)
+  networking.hostName = "tokuOS";
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
+  
+  # TZ.
   time.timeZone = "America/Argentina/Buenos_Aires";
-  i18n.defaultLocale = "es_AR.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
 
+  # WiFi.
+  networking.networkmanager.enable = true;
+  
+  # COSMIC
   services.desktopManager.cosmic.enable = true;
   services.displayManager.cosmic-greeter.enable = true;
 
-  networking.networkmanager.enable = true;
-
+  # Hyprland
+  programs.hyprland.enable = true;
+  
+  # Fonts.
+  fonts.packages = with pkgs; [
+	  nerd-fonts.fira-code
+	  nerd-fonts.space-mono
+	  nerd-fonts.hurmit
+  ];
+  
+  # Normal User.
   users.users.agustin = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" "docker" ];
     shell = pkgs.bash;
   };
 
-  fonts.packages = with pkgs; [
-	  nerd-fonts.fira-code
-	  nerd-fonts.space-mono
-	  nerd-fonts.hurmit
-  ];
-
-  # Unfree pkgs
-  nixpkgs.config.allowUnfree = true;
-
-  # Flakes & stuff
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  programs.hyprland.enable = true;
-
+  # User for testing.
   users.users.arusso = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" "docker" ];
     shell = pkgs.bash;
   };
 
+  # Unfree pkgs.
+  nixpkgs.config.allowUnfree = true;
+
+  # Flakes & stuff.
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Global pkgs.
   environment.systemPackages = with pkgs; [
     git
     gh
@@ -48,21 +57,27 @@
     zip
     unzip
   ];
+  
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
 
-  # Flatpak enabled
+  # SYS Version.
+  system.stateVersion = "25.05";
+  
+  
+  ### ETC's ###
+
+  # Flatpak enabled.
   services.flatpak.enable = true;
 
-  # Docker
+  # Docker.
     virtualisation.docker = {
     enable = true;
   };
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  #Virtualization with LibVirt
+  #Virtualization with LibVirt.
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
 
-  system.stateVersion = "25.05";
+  ### ETC's end ###
 }
--- users/arusso/programs.nix
+++ users/arusso/programs.nix
@@ -12,15 +12,10 @@
 
   programs.git = {
     enable = true;
-    userName = "rssAgus";
-    userEmail = "arusso@fixear.net";
   };
+    
 
   programs.starship = {
     enable = true;
-    settings = {
-      add_newline = false;
-      format = "$directory $git_branch $character";
-    };
   };
 }

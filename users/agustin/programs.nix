9267429294c85dc26bfd81552a72e712b1d43462
rssAgus <arusso@fixear.net>
2025-07-21 23:14:45 -0300

    Moved my packages to agustin

--- users/agustin/programs.nix
+++ users/agustin/programs.nix
@@ -3,9 +3,36 @@
 {
   home.packages = with pkgs; [
     bash
-    neofetch
-    starship
-    helix
+    vscode
+    git
+    gh	
+    spotify
+    nodejs		
+    openvpn
+    openfortivpn
+    unzip
+    zip
+    nuclei
+    zap
+    gobuster
+    go
+    fastfetch
+    pkgs.starship	
+    arp-scan	
+    nmap
+    python3
+    john
+    theharvester
+    dnsutils
+    python313Packages.pip
+    pipx
+    tree
+    qemu
+    google-chrome
+    discord
+    kitty
+    cmatrix
+    btop
   ];
 
   programs.bash.enable = true;
